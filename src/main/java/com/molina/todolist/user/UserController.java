27abd0f3b72920f574472c4030e1eb0bd7987da5
ElaineMolina <elainemolina10@gmail.com>
2023-10-12 14:33:01 -0300

    validar name - response entity

--- src/main/java/com/molina/todolist/user/UserController.java
+++ src/main/java/com/molina/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package com.molina.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +15,15 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUserName(userModel.getUserName());
+
+        if (user != null){
+            System.out.println("Usuário já existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
+
