d859b7cfdfd006666abcb522dcd214400876a0ed
ElaineMolina <elainemolina10@gmail.com>
2023-10-12 14:13:20 -0300

    tabela usuário e repository

--- src/main/java/com/molina/todolist/user/UserModel.java
+++ src/main/java/com/molina/todolist/user/UserModel.java
@@ -1,13 +1,28 @@
 package com.molina.todolist.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalTime;
+import java.util.UUID;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
     private String name;
     private String userName;
     private String password;
+
+    @CreationTimestamp
+    private LocalTime createdAt;
 
     public UserModel() {
     }
